ac8a0f1d056598c027481dddfcc207dab5735e99
Alexander Kharitonov <a.kharitonov@pixonic.ru>
2018-06-22 11:53:35 +0300

    Initial commit

---
+++ src/main/java/com/pixonic/simplesystemdservice/Systemd.java
@@ -0,0 +1,21 @@
+package com.pixonic.simplesystemdservice;
+
+import java.io.IOException;
+
+public class Systemd {
+    public static void sendReady() {
+        try {
+            Process process = new ProcessBuilder("systemd-notify", "--ready")
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .redirectError(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("Can't notify systemd");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Can't notify systemd", e);
+        }
+    }
+}
